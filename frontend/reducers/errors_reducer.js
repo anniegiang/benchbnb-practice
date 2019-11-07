672396f5e5488bff82af727cf5cd4274d98de72e
Annie Giang <annieg4123@gmail.com>
2019-11-07 14:57:18 -0800

    Create errors reducer

---
+++ frontend/reducers/errors_reducer.js
@@ -0,0 +1,6 @@
+import { combineReducers } from "redux";
+import session from "./session_errors_reducer";
+
+const errorsReducer = combineReducers({
+  session
+});
--- frontend/reducers/session_errors_reducer.js
+++ frontend/reducers/session_errors_reducer.js
@@ -14,3 +14,5 @@ const sessionErrors = (state = [], action) => {
       return state;
   }
 };
+
+export default sessionErrors;
