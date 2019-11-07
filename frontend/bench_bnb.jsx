e9759a5da666f5c74102d601a1a298753a29e50d
Annie Giang <annieg4123@gmail.com>
2019-11-07 13:19:07 -0800

    Create session util api

--- frontend/bench_bnb.jsx
+++ frontend/bench_bnb.jsx
@@ -1,7 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { login, logout, signup } from "./util/session_api_util";
 
 document.addEventListener("DOMContentLoaded", () => {
   const root = document.getElementById("root");
+
+  // test
+  window.login = login;
+  window.logout = logout;
+  window.signup = signup;
+
   ReactDOM.render(<h1>BenchBnb</h1>, root);
 });
