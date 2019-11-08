404e8566519094a5c3d24261f244d401d72bbd65
Annie Giang <annieg4123@gmail.com>
2019-11-07 21:31:12 -0800

    Change lng to long

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Bench.create([
+  {description: 'Alamo Square', lat: 37.776414, long: -122.431583},
+  {description: 'Civic Center', lat: 37.781188, long: -122.418680},
+  {description: 'Russian Hill', lat: 37.798093, long: -122.413749},
+  {description: "Fisherman's Wharf", lat: 37.807269, long: -122.419514},
+])
