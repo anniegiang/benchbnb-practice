ab4374577a688c66078e7cd90cc845547836500d
Annie Giang <annieg4123@gmail.com>
2019-11-07 21:09:21 -0800

    Create Bench migration and model

---
+++ app/models/bench.rb
@@ -0,0 +1,14 @@
+# == Schema Information
+#
+# Table name: benches
+#
+#  id          :integer          not null, primary key
+#  description :string           not null
+#  lat         :float            not null
+#  long        :float            not null
+#
+
+class Bench < ApplicationRecord
+  validates :description, :lat, :long, presence :true
+
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_07_192854) do
+ActiveRecord::Schema.define(version: 2019_11_08_050307) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "benches", force: :cascade do |t|
+    t.string "description", null: false
+    t.float "lat", null: false
+    t.float "long", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "username", null: false
